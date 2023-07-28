94b0d6cdd5bb5938526975bb5ebef98410766bb8
Slavi15 <slavirezashkiofficial@gmail.com>
2023-07-28 15:05:50 +0300

    Update final links

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -5,7 +5,7 @@ import img from '../assets/park.jpg';
 const FinishScreen = () => {
     function handleClick() {
         sessionStorage.clear();
-        window.location.href = 'https://slavi15.github.io/Prosveta-Maze-Adventurer/#/heroes';
+        window.location.href = 'https://slavi15.github.io/Maze-Adventurer/#/heroes';
     };
 
     return (
